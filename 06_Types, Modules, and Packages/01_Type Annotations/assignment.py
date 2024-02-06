150d6789499ed2208e601c688e873b601e1d8930
David Peterson <peterdav000@warren.k12.in.us>
2024-02-06 12:28:34 -0500

    6.1.1

--- 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
+++ 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
@@ -1,6 +1,3 @@
-#add type annotation on the functions and variables
-#variables types are float, function return type is a float
-
 def average(a, b):
     return (a + b) / 2
 
---
+++ 06_Types, Modules, and Packages/02_Modules, Import and Math/assignment.py
@@ -0,0 +1,8 @@
+import math
+
+leg1 = float(input("Enter the value of leg 1: "))
+leg2 = float(input("Enter the value of leg 2:"))
+
+hyp = math.hypot (7,8)
+
+print("The hypotenuse is", hyp)
