e19fb9ca2e06a0dce2ab12a350f1dc422e249627
David Peterson <peterdav000@warren.k12.in.us>
2024-03-08 12:09:43 -0500

    assignment

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -14,18 +14,34 @@ class Rectangle:
     def get_height(self) -> float:
         return self.__height
 
-    # YOUDO the get_base method
+    def get_base(self) -> float:
+        return self.__base 
 
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
 
-    # Youdo get_area method
+    def get_area(self) -> float:
+        return self.__base * self.__height       # Youdo get_area method
     
     def __str__(self) -> str:
         # Rectangle of base:3, height:4
-        return "Rectangle of base:" + self.__base + ", height:" + self.__height
-        pass
+        return "Rectangle of base:" + str(self.__base) + ", height:" + str(self.__height)
+        
+rectangle1 = Rectangle(3, 4)
+print(rectangle1.get_base())
+print(rectangle1.get_height())
+print(rectangle1.get_area())
+print(rectangle1.get_perimeter())
 
+print(rectangle1)
+
+rectangle2 = Rectangle(5, 6)
+print(rectangle2.get_base())
+print(rectangle2.get_height())
+print(rectangle2.get_area())
+print(rectangle2.get_perimeter())
+
+print(rectangle2)
 
 # YOUDO>  create two rectangles.  print their base, height, perimeter, and area
 # using only the methods not the fields/property/attributes
