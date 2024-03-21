67c793e4757dabd41a9e612bff593ea9fc5ee7de
David Peterson <peterdav000@warren.k12.in.us>
2024-03-21 12:07:31 -0400

    assignemnt p4

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,10 +1,11 @@
 file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\assignment.py"
 
-try:
-    stream = open(file_path)
-    #YOUDO print to console using stream.read()
+stream = open(file_path)
+print(stream.read())
+print(stream.close())
+    #YOUDO print to consule using stream.read()
     #YOUDO don't forget to close the stream
-except:
-    pass
+    # except Exception as e:
+print('An error ocurred:, e')
 ##YOUDO rest of the program for reading and printing to the console
 ##remember pass in file_path to open 
